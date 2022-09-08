0bf569055a3e4046483ab7813671b8f47eaf164d
alfredt44 <alfredt44@yahoo.com>
2022-09-08 12:10:22 +0700

    commit pertama

---
+++ Kotlin Fundamental/Data Type/Character/src/App.kt
@@ -0,0 +1,12 @@
+// main function
+fun main() {
+    var vocal = 'A'
+
+    println("Vocal1 " + vocal++)
+    println("Vocal2 " + vocal++)
+    println("Vocal3 " + vocal++)
+    println("Vocal4 " + vocal--)
+    println("Vocal5 " + vocal--)
+    println("Vocal6 " + vocal--)
+    println("Vocal7 " + vocal--)
+}
